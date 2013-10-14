ffd7d570bc9775d273d6d915a889b50230a32a0b
lyaunzbe <lyaunzon.b@gmail.com>
2013-10-14 19:29:12 -0400

    Improved matching algorithm

--- soundEngine.py
+++ soundEngine.py
@@ -1,58 +1,64 @@
-import sys,os,scipy,wave, struct
+import sys, wave, struct
 import numpy as np
-
+ 
 def main(file1, file2):
-  # If either file1 or file2 aren't WAVE, wave.open() throws an error
-  try:
-    sound1 = wave.open(file1, 'r')
-    sound2 = wave.open(file2, 'r')
-    size1 = sound1.getnframes()
-    size2 = sound2.getnframes()
-    data1 = sound1.readframes(size1)
-    data2 = sound2.readframes(size2)
-    sound1.close()
-    sound2.close()
-    data1 = struct.unpack('{n}h'.format(n=len(data1)/2), data1)
-    data2 = struct.unpack('{n}h'.format(n=len(data2)/2), data2)
-    data1 = np.array(data1)
-    data2 = np.array(data2)
-    # w1, w2 contain fft of each sound file
-    # freqs1, freqs2 contain DFT sample frequencies
-    w1 = np.fft.fft(data1)
-    w2 = np.fft.fft(data2)
-    freqs1 = np.fft.fftfreq(len(w1))
-    freqs2 = np.fft.fftfreq(len(w2))
-    # need to use some other comparison method than np.any()
-    # Also we need serious print statements
-    #compare(freqs1, freqs2, w1, w2)
-    x = set(w1)
-    y = set(w2)
-    if x.issubset(y) or y.issubset(x):
-      print "MATCH"
-    else:
-      print "NO MATCH"
-  except (wave.Error):
-    print "Improper command line use.  Both files must be WAVE type."
-
-# Takes a very long time to compare any significantly-sized audio samples.
-# Perhaps we should try the divide-and-conquer approach
-def compare(f1, f2, w1, w2):
-  similarities = 0
-  for freq in f1:
-    if freq in f2:
-      similarities += 1
-  if similarities/len(f1) > 0.65 or similarities/len(f2) > 0.65:
-    print "Greater than a 65% match in frequencies"
-  fftSimilarities = 0
-  for fft in w1:
-    if fft in w2:
-      fftSimilarities += 1
-  if fftSimilarities/len(w1) > 0.65 or fftSimilarities/len(f2) > 0.65:
-    print "Greater than 65% match in fft values"
-
-if __name__ == '__main__':
-  if len(sys.argv) != 3:
-    print 'Error: Proper command line usage is "./p4500 <pathname> <pathname>"\n'
+    # If either file1 or file2 aren't WAVE, wave.open() throws an error
+    try:
+        sound1 = wave.open(file1, 'r')
+        sound2 = wave.open(file2, 'r')
+        size1 = sound1.getnframes()
+        size2 = sound2.getnframes()
+        data1 = sound1.readframes(size1)
+        data2 = sound2.readframes(size2)
+        sound1.close()
+        sound2.close()
+        data1 = struct.unpack('{n}h'.format(n=len(data1)/2), data1)
+        data2 = struct.unpack('{n}h'.format(n=len(data2)/2), data2)
+        data1 = np.array(data1)
+        data2 = np.array(data2)
+        # w1, w2 contain fft of each sound file
+        # freqs1, freqs2 contain DFT sample frequencies
+        w1 = np.fft.fft(data1)
+        w2 = np.fft.fft(data2)
+        freqs1 = np.fft.fftfreq(len(w1))
+        freqs2 = np.fft.fftfreq(len(w2))
+        x = set(w1)
+        y = set(w2)
+        matched = 0
+        if len(w1) < len(w2):
+          print len(w1), len(w2)
+          compare(w1, w2, freqs1, freqs2)
+        else:
+          compare(w2, w1, freqs2, freqs1)
+    except (wave.Error):
+        print "Improper command line use.  Both files must be WAVE type."
+ 
+ 
+ 
+# Compare two sets of FFT and corresponding frequencies
+# Look at the strongest FFT values,
+#  get the frequencies corresponding to those values
+#  check if those frequencies are a subset of the frequencies from
+#  the other audio file
+# The shorter file drives this 
+def compare(w1, w2, f1, f2):
+  sortedFFT = np.sort(w1, kind='mergesort')
+  topFFT = sortedFFT[len(sortedFFT) - 20:]
+  topFFTindices = []
+  topFreqs = []
+  for val in topFFT:
+    topFFTindices.append(np.where(w1==val)[0][0])
+  for i in topFFTindices:
+    topFreqs.append(f1[i])
+  setFreqs = set(topFreqs)
+  if setFreqs.issubset(f2):
+    print "MATCH"
   else:
-    main(sys.argv[1], sys.argv[2])
-
+    print "NO MATCH"
+ 
+ 
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print 'Error: Proper command line usage is "./p4500 <pathname> <pathname>"\n'
+    else:
+        main(sys.argv[1], sys.argv[2])
