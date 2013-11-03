30b5eeedbbf7a1dc3e081c68d006c27052907317
rsawyer <rsawyer@darktower.ccs.neu.edu>
2013-11-03 17:22:49 -0500

    Applied hamming window, changed windows to overlap

--- soundEngine.py
+++ soundEngine.py
@@ -6,7 +6,7 @@ Alec Schwartz
 Eduardo Romeiro
 """
 
-import sys, wave, struct
+import sys, wave, struct, math
 import numpy as np
  
 def main(file1, file2):
@@ -32,30 +32,65 @@ def main(file1, file2):
       #Unpack strings into an array of values
       data1 = struct.unpack('{n}h'.format(n=len(data1)/2), data1)
       data2 = struct.unpack('{n}h'.format(n=len(data2)/2), data2)
-      #Calculate samples per frame
-      samplesPerFrame1 = 0.03*frate1   # size  of frame in samples. 30 ms frame size
+      # Size of frame in samples. 30 ms frame size
+      samplesPerFrame1 = 0.03*frate1
       samplesPerFrame2 = 0.03*frate2
       framedSignal1 = []
       framedSignal2 = []
       i = 0
+      # We want a quarter of the frame size to overlap for each signal
+      overlap1 = samplesPerFrame1 * 0.25
+      overlap2 = samplesPerFrame2 * 0.25
       # Frame signals
-      # TODO: apply hamming, overlap windows
       while i < len(data1):
         framedSignal1.append(data1[i:int(i + samplesPerFrame1)])
-        framedSignal2.append(data2[i:int(i + samplesPerFrame2)])
-        i = int(i + samplesPerFrame1)
-      ffts1 = []
-      ffts2 = []
+        i = int(i + samplesPerFrame1 - overlap1)
+      j = 0
+      while j < len(data1):
+        framedSignal2.append(data2[j:int(j + samplesPerFrame2)])
+        j = int(j + samplesPerFrame2 - overlap2)
+
       #Calculate FFT values from framed signals
-      for signal in framedSignal1:
-        if (len(signal) == 0): break
-        ffts1.append(np.fft.fft(signal))
-      for signal in framedSignal2:
-        if (len(signal) == 0): break
-        ffts2.append(np.fft.fft(signal))
-      # Setup hamming wind0ow 
+      # Setup hamming window 
       hammingWindow1 = np.hamming(samplesPerFrame1)
       hammingWindow2 = np.hamming(samplesPerFrame2)
+      # Initialize two arrays for the application of the hamming window
+      hammedSignal1, hammedSignal2 = [], []
+      # For each frame in each signal, apply the hamming window
+      # If the frame size is less than the hamming window, pad the frame with zeros
+      for frame in framedSignal1:
+        if len(frame) < len(hammingWindow1):
+          frame = list(frame)
+          frame[len(frame):len(hammingWindow1)] = [0] * (len(hammingWindow1) - len(frame))
+          frame = tuple(frame)
+        hammedSignal1.append(frame * hammingWindow1)
+      for frame in framedSignal2:
+        if len(frame) < len(hammingWindow2):
+          frame = list(frame)
+          frame[len(frame):len(hammingWindow1)] = [0] * (len(hammingWindow1) - len(frame))
+          frame = tuple(frame)
+        hammedSignal2.append(frame * hammingWindow2)
+      # Initialize arrays for the fourier transform and subsequent mapping
+      # to the power spectrum of the signal
+      ffts1 = []
+      ffts2 = []
+      powerSpectrum1 = []
+      powerSpectrum2 = []
+      # Compute the fourier transform of each frame, then map the transform
+      # to the power spectrum of the signal
+      for signal in hammedSignal1:
+        if (len(signal) == 0): break
+        fft = np.fft.fft(signal)
+        power = abs(fft) ** 2
+        ffts1.append(fft)
+        powerSpectrum1.append(power)
+      for signal in hammedSignal2:
+        if (len(signal) == 0): break
+        fft = np.fft.fft(signal)
+        power = abs(fft) ** 2
+        ffts2.append(fft) 
+        powerSpectrum2.append(power)
+  
       #print len(framedSignal1)
       #print len(data1), len(data2)
       data1 = np.array(data1)
@@ -75,8 +110,15 @@ def main(file1, file2):
         compare(w2, w1, freqs2, freqs1)
     except (wave.Error):
       sys.stderr.write("ERROR: Improper command line use.  Both files must be WAVE type.")
-      exit(-1) 
- 
+      sys.exit(-1) 
+
+
+def freqToMel(freq):
+  return 1127.01048 * math.log(1 + freq / 700.0)
+
+def melToFreq(mel):
+  return 700 * (math.exp(freq / 1127.01048 - 1))
+
 # Compare two sets of FFT and corresponding frequencies
 # Look at the strongest FFT values,
 #  get the frequencies corresponding to those values
@@ -104,6 +146,6 @@ def exit(val):
 if __name__ == '__main__':
   if len(sys.argv) != 3:
     sys.stderr.write('Error: Proper command line usage is "./p4500 <pathname> <pathname>"\n')
-    exit(-1)
+    sys.exit(-1)
   else:
     main(sys.argv[1], sys.argv[2])
