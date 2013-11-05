72970f4f6c1dd18d40099776993c151724539cb3
rsawyer <rsawyer@shogunwarriors.ccs.neu.edu>
2013-11-04 20:27:46 -0500

    Added mel-filterbank, added mp3 conversion.  Still have to properly compare files.

--- soundEngine.py
+++ soundEngine.py
@@ -6,118 +6,160 @@ Alec Schwartz
 Eduardo Romeiro
 """
 
-import sys, wave, struct, math
+
+# TODO: fix MFCC/filterbank.  Properly compare values
+
+import sys, wave, struct, math, subprocess
 import numpy as np
- 
+
+LAME = '/course/cs4500f13/bin/lame'
+
+# Read in both files and extract the number of audio frames, frame
+# rate, and read all audio frames, then close the file and perform
+# a Fast Fourier Transform on the files
 def main(file1, file2):
     # If either file1 or file2 aren't WAVE, wave.open() throws an error
     try:
-      # Read in both files and extract the number of audio frames, frame
-      # rate, and read all audio frames, then close the file and perform
-      # a Fast Fourier Transform on the files
       sound1 = wave.open(file1, 'r')
-      sound2 = wave.open(file2, 'r')
-      # Get frames of length n
-      size1 = sound1.getnframes()
-      size2 = sound2.getnframes()
-      # read number of frames of size n
-      data1 = sound1.readframes(size1)
-      data2 = sound2.readframes(size2)
-      #instantiate some framerates
-      frate1 = sound1.getframerate()
-      frate2 = sound2.getframerate()
-      #close audio files 
-      sound1.close()
-      sound2.close()
-      #Unpack strings into an array of values
-      data1 = struct.unpack('{n}h'.format(n=len(data1)/2), data1)
-      data2 = struct.unpack('{n}h'.format(n=len(data2)/2), data2)
-      # Size of frame in samples. 30 ms frame size
-      samplesPerFrame1 = 0.03*frate1
-      samplesPerFrame2 = 0.03*frate2
-      framedSignal1 = []
-      framedSignal2 = []
-      i = 0
-      # We want a quarter of the frame size to overlap for each signal
-      overlap1 = samplesPerFrame1 * 0.25
-      overlap2 = samplesPerFrame2 * 0.25
-      # Frame signals
-      while i < len(data1):
-        framedSignal1.append(data1[i:int(i + samplesPerFrame1)])
-        i = int(i + samplesPerFrame1 - overlap1)
-      j = 0
-      while j < len(data1):
-        framedSignal2.append(data2[j:int(j + samplesPerFrame2)])
-        j = int(j + samplesPerFrame2 - overlap2)
-
-      #Calculate FFT values from framed signals
-      # Setup hamming window 
-      hammingWindow1 = np.hamming(samplesPerFrame1)
-      hammingWindow2 = np.hamming(samplesPerFrame2)
-      # Initialize two arrays for the application of the hamming window
-      hammedSignal1, hammedSignal2 = [], []
-      # For each frame in each signal, apply the hamming window
-      # If the frame size is less than the hamming window, pad the frame with zeros
-      for frame in framedSignal1:
-        if len(frame) < len(hammingWindow1):
-          frame = list(frame)
-          frame[len(frame):len(hammingWindow1)] = [0] * (len(hammingWindow1) - len(frame))
-          frame = tuple(frame)
-        hammedSignal1.append(frame * hammingWindow1)
-      for frame in framedSignal2:
-        if len(frame) < len(hammingWindow2):
-          frame = list(frame)
-          frame[len(frame):len(hammingWindow1)] = [0] * (len(hammingWindow1) - len(frame))
-          frame = tuple(frame)
-        hammedSignal2.append(frame * hammingWindow2)
-      # Initialize arrays for the fourier transform and subsequent mapping
-      # to the power spectrum of the signal
-      ffts1 = []
-      ffts2 = []
-      powerSpectrum1 = []
-      powerSpectrum2 = []
-      # Compute the fourier transform of each frame, then map the transform
-      # to the power spectrum of the signal
-      for signal in hammedSignal1:
-        if (len(signal) == 0): break
-        fft = np.fft.fft(signal)
-        power = abs(fft) ** 2
-        ffts1.append(fft)
-        powerSpectrum1.append(power)
-      for signal in hammedSignal2:
-        if (len(signal) == 0): break
-        fft = np.fft.fft(signal)
-        power = abs(fft) ** 2
-        ffts2.append(fft) 
-        powerSpectrum2.append(power)
-  
-      #print len(framedSignal1)
-      #print len(data1), len(data2)
-      data1 = np.array(data1)
-      data2 = np.array(data2)
-      # w1, w2 contain fft of each sound file
-      # freqs1, freqs2 contain DFT sample frequencies
-      w1 = np.fft.fft(data1)
-      w2 = np.fft.fft(data2)
-      #print len(w1), len(w2)
-      freqs1 = np.fft.fftfreq(len(w1))
-      freqs2 = np.fft.fftfreq(len(w2))
-      # compare the two ffts and frequency spectrums, with the smaller file
-      # driving the comparison
-      if len(w1) < len(w2):
-        compare(w1, w2, freqs1, freqs2)
-      else:
-        compare(w2, w1, freqs2, freqs1)
     except (wave.Error):
-      sys.stderr.write("ERROR: Improper command line use.  Both files must be WAVE type.")
-      sys.exit(-1) 
+      try:
+        command = LAME + ' ' + file1 + ' /tmp/sound1.wav'
+        sound1 = subprocess.check_call(command)
+        # TODO: read new file as WAVE
+      except (CalledProcessError):
+        sys.stderr.write('ERROR: Improper file type.  Both files must be WAVE or MP3.\n')
+        sys.exit(-1)
+    try:
+      sound2 = wave.open(file2, 'r')
+    except (wave.Error):
+      try:
+        command = LAME + ' ' + file1 + ' /tmp/sound2.wav'
+        sound1 = subprocess.check_call(command)
+        # TODO: read new file as WAVE
+      except (CalledProcessError):
+        sys.stderr.write('ERROR: Improper file type.  Both files must be WAVE or MP3.\n')
+        sys.exit(-1) 
+    # Get frames of length n
+    size1 = sound1.getnframes()
+    size2 = sound2.getnframes()
+    # read number of frames of size n
+    data1 = sound1.readframes(size1)
+    data2 = sound2.readframes(size2)
+    #instantiate some framerates
+    frate1 = sound1.getframerate()
+    frate2 = sound2.getframerate()
+    #close audio files 
+    sound1.close()
+    sound2.close()
+    #Unpack strings into an array of values
+    data1 = struct.unpack('{n}h'.format(n=len(data1)/2), data1)
+    data2 = struct.unpack('{n}h'.format(n=len(data2)/2), data2)
+    # Size of frame in samples. 30 ms frame size
+    samplesPerFrame1 = 0.03*frate1
+    samplesPerFrame2 = 0.03*frate2
+    framedSignal1 = []
+    framedSignal2 = []
+    i = 0
+    # We want a quarter of the frame size to overlap for each signal
+    overlap1 = samplesPerFrame1 * 0.25
+    overlap2 = samplesPerFrame2 * 0.25
+    # Frame signals
+    while i < len(data1):
+      framedSignal1.append(data1[i:int(i + samplesPerFrame1)])
+      i = int(i + samplesPerFrame1 - overlap1)
+    j = 0
+    while j < len(data1):
+      framedSignal2.append(data2[j:int(j + samplesPerFrame2)])
+      j = int(j + samplesPerFrame2 - overlap2)
+    #Calculate FFT values from framed signals
+    # Setup hamming window 
+    hammingWindow1 = np.hamming(samplesPerFrame1)
+    hammingWindow2 = np.hamming(samplesPerFrame2)
+    # Apply the hamming window to each signal
+    hammedSignal1 = applyHammingWindow(framedSignal1, hammingWindow1)
+    hammedSignal2 = applyHammingWindow(framedSignal2, hammingWindow2)
+    # Get the Fourier Transform and power spectrum of each signal
+    ffts1, powerSpectrum1 = getFFTandPower(hammedSignal1)
+    ffts2, powerSpectrum2 = getFFTandPower(hammedSignal2)
+    #compareEuclid(ffts1, ffts2)
+    #compareEuclid(powerSpectrum1, powerSpectrum2)
+    frameFrequencies1 = np.fft.fftfreq(int(samplesPerFrame1))
+    frameFrequencies2 = np.fft.fftfreq(int(samplesPerFrame2))
+    compare(ffts1[0], ffts2[0], frameFrequencies1, frameFrequencies2)
+    melFilterBank = filterbank(20, len(powerSpectrum1[0]), frate1)
+    print np.shape(melFilterBank)
+    print np.shape(powerSpectrum1[0])
 
 
+def getFFTandPower(signal):
+  fftValues, powerValues = [], []
+  for frame in signal:
+    if (len(frame) == 0): break
+    fft = np.fft.fft(frame)
+    power = abs(fft) ** 2
+    fftValues.append(fft)
+    powerValues.append(power)
+  return (fftValues, powerValues)
+
+# For each frame in each signal, apply the hamming window
+# If the frame size is less than the hamming window, pad the frame with zeros
+def applyHammingWindow(signal, hammingWindow):
+  result = []
+  for frame in signal:
+    if len(frame) < len(hammingWindow):
+      frame = list(frame)
+      frame[len(frame):len(hammingWindow)] = [0] * (len(hammingWindow) - len(frame))
+      frame = tuple(frame)
+    result.append(frame * hammingWindow)
+  return result
+
+# Computer the Mel-Frequency filterbank.  Once that is done, it can be
+# applied to each window to get the Mel-Frequency Cepstral Coefficients
+# of each frame
+def filterbank(nfilt=20,nfft=512,samplerate=44100,lfreq=0,hfreq=None):
+  hfreq = hfreq or (samplerate/2)
+  
+  lmel = freqToMel(lfreq)
+  hmel = freqToMel(hfreq)
+  melpts = np.linspace(lmel, hmel, nfilt+2)
+  #print melpts
+  fftbin = np.floor((nfft+1)*melToFreq(melpts)/samplerate)
+  #print fftbin
+  fbank = np.zeros([nfilt, nfft/2+1])
+
+  for x in xrange(0, nfilt):
+    for y in xrange(int(fftbin[x]),int(fftbin[x+1])):
+      #print (y - fftbin[x])/(fftbin[x+1]-fftbin[x])
+      fbank[x,y] = (y - fftbin[x])/(fftbin[x+1]-fftbin[x])
+    for y in xrange(int(fftbin[x+1]),int(fftbin[x+2])):
+      fbank[x,y] = (fftbin[x+2]-y)/(fftbin[x+2]-fftbin[x+1])
+  return fbank
+ 
 def freqToMel(freq):
-  return 1127.01048 * math.log(1 + freq / 700.0)
+  return 2595 * np.log10(1+freq/700.0)
 
 def melToFreq(mel):
-  return 700 * (math.exp(freq / 1127.01048 - 1))
+  return 700 * (10**(mel / 2595.0 - 1))
+
+# Input: two two-dimensional arrays of equal length
+# Compute the euclidean distance between each sub-array
+def compareEuclid(ray1, ray2):
+  result = []
+  if len(ray1) != len(ray2):
+    return -1
+  for i in range(len(ray1)):
+    result.append(eDist(ray1[i], ray2[i]))
+  print result
+
+# Compute the euclidean distance between two arrays
+def eDist(vec1, vec2):
+  result = 0
+  if len(vec1) != len(vec2):
+    return -1
+  for i in range(len(vec1)):
+    dist = (vec1[i] - vec2[i]) ** 2
+    result = result + dist
+  return result
 
 # Compare two sets of FFT and corresponding frequencies
 # Look at the strongest FFT values,
@@ -139,13 +181,18 @@ def compare(w1, w2, f1, f2):
     print "MATCH"
   else:
     print "NO MATCH"
+  sys.exit(0)
 
 def exit(val):
   return val
- 
+
 if __name__ == '__main__':
-  if len(sys.argv) != 3:
-    sys.stderr.write('Error: Proper command line usage is "./p4500 <pathname> <pathname>"\n')
+  if len(sys.argv) != 5:
+    sys.stderr.write('ERROR: Proper command line usage is "./p4500 -f <pathname> -f <pathname>"\n')
     sys.exit(-1)
   else:
-    main(sys.argv[1], sys.argv[2])
+    if sys.argv[1] != '-f' or sys.argv[3] != '-f':
+      sys.stderr.write('ERROR: Proper command line usage is "./p4500 -f <pathname> -f <pathname>')
+      sys.exit(-1)
+    else:
+      main(sys.argv[2], sys.argv[4])
